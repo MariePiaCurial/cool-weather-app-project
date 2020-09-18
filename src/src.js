c592a6eb8426b25e31fa86147eb5a11c076fbe32
MariaPiaC <67664176+MariePiaCurial@users.noreply.github.com>
2020-09-18 13:02:31 +0200

    Update src: Create function showPosition
    -  API response = city name if retrieve current position
    - Display current temperature in current position (Data API)
    -  Display current weather description & elements in current position (Data API)
    - Link icon & background to weather in current position (Data API)

--- src/src.js
+++ src/src.js
@@ -1,6 +1,8 @@
 let now = new Date();
 let hours = now.getHours();
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let lat;
+let lon;
 
 // 1) Create function currentTime : display current Date, Hour
 
@@ -106,5 +108,96 @@ function showTemperature(event) {
     rgba(233, 197, 168,0.8),
     rgba(212, 82, 1,0.8)
   ), url('assets/${icon}.jpg')`;
+  });
+
+  // display forecast temp if ask city
+
+  function ForecastedTemp() {
+    axios.get(apiUrl).then((response) => {
+      let latitude = response.data.coord.lat;
+      let longitude = response.data.coord.lon;
+      let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
+      axios.get(url).then((response) => {
+        let morningForecast = Math.round(response.data.daily[0].temp.morn);
+        let morningForecastTemp = document.querySelector(
+          ".morningForecastTemp"
+        );
+        morningForecastTemp.innerHTML = `${morningForecast}`;
+
+        let afternoonForecast = Math.round(response.data.daily[0].temp.eve);
+        let afternoonForecastTemp = document.querySelector(
+          ".afternoonForecastTemp"
+        );
+        afternoonForecastTemp.innerHTML = `${afternoonForecast}`;
+        let nightforecast = Math.round(response.data.daily[0].temp.night);
+        let nightforecastTemp = document.querySelector(".nightforecastTemp");
+        nightforecastTemp.innerHTML = `${nightforecast}`;
+      });
+    });
+  }
+  ForecastedTemp();
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", showTemperature);
+
+// 3) Display current temperature in current position - navigator reference
+
+function showPosition(position) {
+  //  API response = city name if retrieve current position
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(url).then((response) => {
+    let city = response.data.name;
+    document.querySelector("h1").innerHTML = city;
+
+    // Display current temperature in current position (Data API)
+
+    let temperature = Math.round(response.data.main.temp);
+    let weather = document.querySelector("#temperature");
+    weather.innerHTML = temperature;
+
+    // Display current weather description & elements in current position (Data API)
+
+    let description = response.data.weather[0].description;
+    let descriptionWeather = document.querySelector("p");
+    descriptionWeather.innerHTML = description;
+
+    let humidity = response.data.main.humidity;
+    let currentHumidity = document.querySelector("#humidity");
+    currentHumidity.innerHTML = `${humidity}%`;
+
+    let wind = Number(response.data.wind.speed);
+    let currentWind = document.querySelector("#wind");
+    currentWind.innerHTML = Math.round(wind * 1.609) + "km/h";
+
+    let cloudiness = response.data.clouds.all;
+    let currentCloudiness = document.querySelector("#cloudiness");
+    currentCloudiness.innerHTML = `${cloudiness}%`;
+
+    // Link icon & background to weather in current position (Data API)
+
+    let icon = response.data.weather[0].icon;
+
+    if (hours <= 6 || hours >= 19) {
+      icon = "01n";
+    } else if (icon === "03d" || icon === "04d") {
+      icon = "02d";
+    } else if (icon == "09d") {
+      icon = "10d";
+    } else {
+      icon = "01d";
+    }
+
+    document.querySelector(".container").style.background = ` linear-gradient(
+    rgba(247, 235, 230,0.8),
+    rgba(233, 197, 168,0.8),
+    rgba(212, 82, 1,0.8)), url('assets/${icon}.jpg')`;
+
+    let currentIcon = document.querySelector(".icon");
+    currentIcon.innerHTML = `<img class='icon' src='http://openweathermap.org/img/wn/${icon}@2x.png' width='30px'></img>`;
   });
 }
