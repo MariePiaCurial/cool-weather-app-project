862908474f97e1b83fc0f51d1294e9a3c7455f9a
MariaPiaC <67664176+MariePiaCurial@users.noreply.github.com>
2020-09-18 12:29:18 +0200

    Update src: create function showTemperature
    - Display current temperature if ask city (API data)
    - Display weather description (API data)
    - Display current weather elements (API data)
    - Link icon to weather description (API data)
    - link background to weather description (API data)

--- src/src.js
+++ src/src.js
@@ -1,7 +1,8 @@
 let now = new Date();
 let hours = now.getHours();
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
 
-// Create function currentTime : display current Date, Hour
+// 1) Create function currentTime : display current Date, Hour
 
 function currentTime() {
   let h3 = document.querySelector("h3");
@@ -42,3 +43,68 @@ function currentTime() {
   h5.innerHTML = `${hours}:${minutes}`;
 }
 currentTime();
+
+// 2) Display current temperature if ask city
+
+function showTemperature(event) {
+  event.preventDefault();
+
+  // display current temperature (API data)
+
+  document.querySelector("h1").innerHTML = document.querySelector(
+    "#city-input"
+  ).value;
+  let city = document.querySelector("h1").innerHTML;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then((response) => {
+    let temperature = Math.round(response.data.main.temp);
+    let weather = document.querySelector("#temperature");
+    weather.innerHTML = temperature;
+
+    // display weather description (API data)
+
+    let description = response.data.weather[0].description;
+    let descriptionWeather = document.querySelector("p");
+    descriptionWeather.innerHTML = description;
+
+    // link current weather element to API data
+
+    let humidity = response.data.main.humidity;
+    let currentHumidity = document.querySelector("#humidity");
+    currentHumidity.innerHTML = `${humidity}%`;
+
+    let wind = Number(response.data.wind.speed);
+    let currentWind = document.querySelector("#wind");
+    currentWind.innerHTML = Math.round(wind * 1.609) + "km/h";
+
+    let cloudiness = response.data.clouds.all;
+    let currentCloudiness = document.querySelector("#cloudiness");
+    currentCloudiness.innerHTML = `${cloudiness}%`;
+
+    // Link icon to weather description (API data)
+
+    let icon = response.data.weather[0].icon;
+    let hours = now.getHours();
+
+    if (hours <= 6 || hours >= 19) {
+      icon = "01n";
+    } else if (icon === "03d" || icon === "04d") {
+      icon = "02d";
+    } else if (icon == "09d") {
+      icon = "10d";
+    } else {
+      icon = "01d";
+    }
+
+    let currentIcon = document.querySelector(".icon");
+    currentIcon.innerHTML = `<img class='icon' src='http://openweathermap.org/img/wn/${icon}@2x.png' width='30px'></img>`;
+
+    //link background to weather description (API data)
+
+    document.querySelector(".container").style.background = `linear-gradient(
+    rgba(247, 235, 230,0.8),
+    rgba(233, 197, 168,0.8),
+    rgba(212, 82, 1,0.8)
+  ), url('assets/${icon}.jpg')`;
+  });
+}
